1b687bbdbbc20fc96cbbedf7adf82f39a4f22a57
Julia <juliafcostads@gmail.com>
2026-03-20 22:37:45 -0300

    Declaração de atributos e anotações para a classe model Funcionário

---
+++ src/main/java/com/example/MiniProjetoFuncionarios/model/Funcionario.java
@@ -0,0 +1,34 @@
+package com.example.MiniProjetoFuncionarios.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "TBFUNCIONARIOS")
+public class Funcionario {
+
+    //criação de primary key autoincrement do banco de dados
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Campo obrigatório.")
+    private String nome;
+
+    //anotação para verificar o formato do email e evitar que seja nulo
+    @Email(message = "E-mail inválido.")
+    @NotBlank(message = "Campo obrigatório.")
+    private String email;
+
+    @NotBlank(message = "Campo obrigatório.")
+    private String cargo;
+
+    @NotNull(message = "Campo obrigatório.")
+    @Positive(message = "Valor inválido.")  //validação para que o salário seja maior que zero
+    private Double salario;
+
+    public Funcionario(){}
+}
