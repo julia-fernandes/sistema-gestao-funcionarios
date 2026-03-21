384b6932a8c04e64193a5aeeda8045920c028842
Julia <juliafcostads@gmail.com>
2026-03-21 20:02:45 -0300

    add restrição para email duplicado: @Column(unique = true, nullable = false, length = 100)

--- src/main/java/com/example/MiniProjetoFuncionarios/model/Funcionario.java
+++ src/main/java/com/example/MiniProjetoFuncionarios/model/Funcionario.java
@@ -21,6 +21,7 @@ public class Funcionario {
     //anotação para verificar o formato do email e evitar que seja nulo
     @Email(message = "E-mail inválido.")
     @NotBlank(message = "Campo obrigatório.")
+    @Column(unique = true, nullable = false, length = 100) //para que o email seja único por registro de funcionário
     private String email;
 
     @NotBlank(message = "Campo obrigatório.")
