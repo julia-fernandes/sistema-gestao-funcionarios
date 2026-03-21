6a1c495b6754baced2ed572a1772904d9b2c1dda
Julia <juliafcostads@gmail.com>
2026-03-20 22:05:14 -0300

    Initial commit

---
+++ src/main/java/com/example/MiniProjetoFuncionarios/MiniProjetoFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.MiniProjetoFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetoFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetoFuncionariosApplication.class, args);
+	}
+
+}
