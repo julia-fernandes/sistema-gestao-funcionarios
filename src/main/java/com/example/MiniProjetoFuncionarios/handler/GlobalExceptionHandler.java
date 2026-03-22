1bee7cc7fde5af1d9c620c9cc0ebf3c80211702c
Julia <juliafcostads@gmail.com>
2026-03-21 22:07:13 -0300

    add handler: tratamento de erros

---
+++ src/main/java/com/example/MiniProjetoFuncionarios/handler/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.example.MiniProjetoFuncionarios.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    //tratamento de erro para validações
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> tratarValidacao(MethodArgumentNotValidException ex){
+        Map<String, String> erros = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(erro ->
+        {
+            erros.put(erro.getField(), erro.getDefaultMessage());
+        });
+        return erros;
+    }
+
+    //tratamento de erros gerais
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Map<String, String> tratarErro(RuntimeException ex){
+        Map<String, String> erro = new HashMap<>();
+        erro.put("erro", ex.getMessage());
+        return erro;
+    }
+}
